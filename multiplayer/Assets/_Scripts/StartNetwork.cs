86fc5b9db1e3383672e5b6d305834d50f89e6837
pip <piporhicyan@gmail.com>
2023-07-11 00:41:01 -0400

    init

---
+++ multiplayer/Assets/_Scripts/StartNetwork.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class StartNetwork : MonoBehaviour
+{
+    public void StartServer()
+    {
+        NetworkManager.Singleton.StartServer();
+    }
+
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient ();
+    }
+}
